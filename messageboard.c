fc6de2ce8b9cfc068a0cfcd6f0d9b939f053f7ec
patcha98 <patcha1@naver.com>
2021-04-29 22:36:27 +0900

    file add

---
+++ messageboard.c
@@ -0,0 +1,53 @@
+//messageboard.c 메인
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "manager.h"
+#define DEBUG_MODE
+
+
+int main(void){
+    #ifdef DEBUG_MODE
+        printf("==> DEBUG MODE\n");
+    #endif
+
+	user userlist[100];
+ 	board boardlist[100];
+	int index = 0;
+	int count = 0, menu;
+	board_count = loadData(boardlist);
+	user_count = loadData(userlist);
+	index = count;
+
+    	while (1){
+        	menu = selectMenu();
+        	if (menu == 0) break; //만약 0이면 취소 
+        	if (menu == 1){
+          		//글 추가 함수
+        	}
+       		else if (menu == 2){
+        		//하나의 글을 출력하는 함수
+        	}
+        	else if (menu == 3){
+        		//글을 수정하는 함수
+        	}
+        	else if (menu == 4){
+        		//글을 목록에서 삭제하는 함수
+        	}
+        	else if (menu == 5){
+        		//함수넣기
+        	}
+		else if (menu == 6){
+        		//함수넣기
+		}
+        	else if (menu == 7){
+        		//함수넣기
+        	}
+        	else if (menu == 8){
+        		//함수넣기
+        	}
+	}
+	printf("종료됨!\n");
+	return 0;
+}
