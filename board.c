9d93dee8c893ea4b6541b58656e74a740d4625fb
patcha98 <patcha1@naver.com>
2021-05-06 18:48:55 +0900

    board.c board.h 2, 3, 4번 함수 구현

--- board.c
+++ board.c
@@ -9,15 +9,54 @@
 // 11. 계정생성(아이디 비번 입력 받아 저장) (입력: 계정 구조체 배열 포인터, 출력:)
     // user_crud.h
 // 2. 게시글 선택후 내용 출력(입력: 게시판 구조체 배열, 출력:) // 리스트 출력 없이 글 번호만 입력
-void printpost(Post p);
+void selectprintpost(Post *p, int count){
+    int num;
+    num = select_Post_DataNo(p, count);
+    printf("제목 : %s \n 작성자 : %s\n  조회수 : %d \n 좋아요수 : %d\n내용 : %s\n", p[num].title, p[num].user, p[num].view, p[num].like, p[num].post);
+}
+
 // 3. 게시판 제목 리스트 출력 (입력: 게시판 구조체 배열, 출력:X)
-void printpostlist(Post *p, int count);
+void printpostlist(Post *p, int count){
+    printf("|   제목   |   작성자  |   조회수  |   좋아요수  |\n"); // 크기 조절 해야함
+    printf("-------------------------------------------------\n");
+    for(int i = 0; i < count; i++){
+        if(p[i].view == -1) continue;
+            printf("%2d  ",i+1);
+        readOnePost(p[i]);
+    }
+}
+void readOnePost(Post p){ // 게시글 리스트 번호 출력
+    printf("제목 : %s  작성자 : %s 조회수 : %d 좋아요수 : %d\n", p.title, p.user, p.view, p.like);// 게시글의 제목 작성자 조회수 추천수 만 표시
+}
+
 // 4. 게시판 글쓰기 (입력: 이용중인 계정 구조체, 카운터(리턴), 게시판 리스트 포인터, 출력: 1 or 0)
-int posting(Post *p, User u);
+int posting(Post *p, User u){
+    int num; //내용 확인할지 물어보기 위해 만듬
+    strcpy(p->user, u.id); // 아이디를 할당시켜 줌.
+    printf("게시글 제목을 적어주세요. : ");
+    scanf("%s", p->title);
+    printf("게시글 내용을 적어주세요. (500자까지 입력 가능합니다.)\n");
+    scanf("%s", p->title);
+    printf("게시글 작성이 완료되었습니다.\n");
+    printf("내용을 확인하시려면 1번 아니면 0번을 입력해주세요 : ");
+    scanf("%d", &num);
+    if(num==1){
+        printf("제목 : %s \n 작성자 : %s\n  조회수 : %d \n 좋아요수 : %d\n내용 : %s\n", p->title, p->user, p->view, p->like, p->post);
+    }
+    else{
+        return 1;
+    }
+    return 1;
+}
+
+
 // 5. 게시글 삭제 (입력: 게시판 구조체 배열 포인터, 계정 구조체, 출력: 1 or 0)
     // crud.h - deletepost
 // 6. 게시글 키워드로 삭제 (입력: 게시판 구조체 배열 포인터,  계정 구조체, 출력:)
-int deletekeyword(Post *p);
+int deletekeyword(Post *p){
+    char search[100];
+    
+}
 // 7. 게시글 수정 (입력: 게시판 구조체 배열 포인터, 출력:)
     // crud.h update
 // 8. 게시글 검색 (제목, 작성자, 내용 키워드)(입력: 게시판 구조체 배열, 출력:)
@@ -65,10 +104,19 @@ void listuser(User *u, int count){
     }
 }
 
-//데이터 선택 함수
+//유저 데이터 선택 함수
 int select_User_DataNo(User *u, int count){
     int no;
  	listuser(u, count);
+ 	printf("번호는? (취소:0)? ");
+ 	scanf("%d", &no);
+    return no;
+}
+
+//포스트 데이터 선택 함수
+int select_Post_DataNo(Post *p, int count){
+    int no;
+ 	listpost(p, count);
  	printf("번호는? (취소:0)? ");
  	scanf("%d", &no);
     return no;
